dc3f184adad66418b4bd33426a3cefadf6bd9d21
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-11 14:36:31 +0000

    Add Functionality to home btn

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
-  const [movie, setMovie] = useState({});
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -20,7 +19,7 @@ const App = () => {
       exact path="/"
       component={MovieList} />
       <Route 
-      path="/movies/:id?"
+      path="/movies/:id"
       component={Movie} />
       {/* <div>Replace this Div with your Routes</div> */}
     </div>
